e0f159bba5d6b691fd1273627d260b3fe9902eee
GrimChips <Ak48umar2@gmail.com>
2025-03-18 23:12:21 +0200

    fixes

--- src/index.php
+++ src/index.php
@@ -24,11 +24,13 @@
     // }
     Navigation();
     ?>
+    <h1>Welcome to the Home Page</h1>
+    <?php
+    include_once __DIR__ . "/assets/templates/footer.php";
+    Footer();
+    ?>
 </body>
-<h1>Welcome to the Home Page</h1>
-<?php
-include_once __DIR__ . "/assets/templates/footer.php";
-Footer();
-?>
+
+
 
 </html>
