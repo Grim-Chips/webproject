deb449f32c2ba87a829023c0f3db581000011d87
GrimChips <Ak48umar2@gmail.com>
2025-03-22 00:27:20 +0200

    fsggsfgsd

--- src/about.php
+++ src/about.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us - Egyptian Red Crescent</title>
-
+    <link rel="stylesheet" href="assets/css/aboutus.css">
 </head>
 
 <body>
