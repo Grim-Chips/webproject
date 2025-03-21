8803c7aba671d2be57db59d14045e70b5619c58f
GrimChips <Ak48umar2@gmail.com>
2025-03-21 23:26:12 +0200

    fix: update navigation link from 'Learn More' to 'History'

--- src/assets/templates/header.php
+++ src/assets/templates/header.php
@@ -10,7 +10,7 @@ function Navigation()
                 <a href="index.php">Home</a>
                 <a href="volunteer.php">Volunteer</a>
                 <a href="about.php">About</a>
-                <a href="learn_more.php">Learn More</a>
+                <a href="history.php">History</a>
                 <a href="trainingcourses.php">training courses</a>
                 <a href="projects_programs.php">projects/programs</a>
             </div>
