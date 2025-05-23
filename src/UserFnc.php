d65064c9b8c649786380080cd6fb2210d4e6dbcd
GrimChips <Ak48umar2@gmail.com>
2025-05-23 21:50:32 +0300

    UwU

--- src/UserFnc.php
+++ src/UserFnc.php
@@ -1,77 +1,72 @@
 <?php
 include "Functins.php";
-$fileName="UsersFile.txt";
+$fileName = "UsersFile.txt";
 function DeleteUser($id)
 {
 	global $fileName;
-	$r=getRowById($fileName,"~",$id);
+	$r = getRowById($fileName, "~", $id);
 	//echo $r;
 	//exit();
-	DeleteRecord($fileName,$r);
+	DeleteRecord($fileName, $r);
 }
-function UpdateUser($id,$Email,$Password,$FullName,$DOB)
+function UpdateUser($id, $Email, $Password, $FullName, $DOB)
 {
 	global $fileName;
-	$record=$id."~".$Email."~".$Password."~".$FullName."~".$DOB."\r\n";
-	$r=getRowById($fileName,"~",$id);
+	$record = $id . "~" . $Email . "~" . $Password . "~" . $FullName . "~" . $DOB . "\r\n";
+	$r = getRowById($fileName, "~", $id);
 	//echo $record ."NEW <br>";
 	//echo $r ."NEW <br>";
-	UpdateRecord($fileName,$record,$r);
-	
+	UpdateRecord($fileName, $record, $r);
+
 }
 function getUserById($Id)
 {
 	global $fileName;
-	$Record=getRowById($fileName,"~",$Id);
-	
-	$ArrayResult=explode("~",$Record);
-	$Result['id']=$ArrayResult[0];
-	$Result['Email']=$ArrayResult[1];
-	$Result['Password']=$ArrayResult[2];
-	$Result['FullName']=$ArrayResult[3];
-	$Result['DOB']=$ArrayResult[4];
+	$Record = getRowById($fileName, "~", $Id);
+
+	$ArrayResult = explode("~", $Record);
+	$Result['id'] = $ArrayResult[0];
+	$Result['Email'] = $ArrayResult[1];
+	$Result['Password'] = $ArrayResult[2];
+	$Result['FullName'] = $ArrayResult[3];
+	$Result['DOB'] = $ArrayResult[4];
 	return $Result;
 }
 function getAllUsers()
 {
 	global $fileName;
-	$R=ListAll($fileName);
+	$R = ListAll($fileName);
 	return $R;
 }
 
 function getAllUsersByKeyWord($KeyWord)
 {
 	global $fileName;
-	$R=SearhKeyword($fileName,$KeyWord);
+	$R = SearhKeyword($fileName, $KeyWord);
 	//echo $R[0] ."Ayman";
 	return $R;
 }
-function Login ($Email,$Password)
+function Login($Email, $Password)
 {
 	global $fileName;
-	if (searchUser($fileName,$Email."~".$Password))
-	{
+	if (searchUser($fileName, $Email . "~" . $Password)) {
 		return true;
-	}
-	else{
+	} else {
 		return false;
 	}
 }
-function addUser($Email,$Password,$FullName,$DOB)
+function addUser($Email, $Password, $FullName, $DOB)
 {
-global $fileName;
-$id=getLastId($fileName,"~")+1;
-$record=$id."~".$Email."~".$Password."~".$FullName."~".$DOB;
-//echo $record;
-if (searchUser($fileName,$Email)==FALSE)
-{
-StoreRecord( $fileName,$record);	
-return true;
-}
-else
-{
-	return FALSE;
-}
+	global $fileName;
+	$id = getLastId($fileName, "~") + 1;
+	$record = $id . "~" . $Email . "~" . $Password . "~" . $FullName . "~" . $DOB;
+	//echo $record;
+	if (searchUser($fileName, $Email) == FALSE) {
+		StoreRecord($fileName, $record);
+		return true;
+	} else {
+		return FALSE;
+	}
 
 
 }
