ce8d6b5b6ef3a99699cc1ce5bd2a26d42f7b121e
GrimChips <Ak48umar2@gmail.com>
2025-05-23 22:42:23 +0300

    Revert "cooomit"
    This reverts commit 78cc5d39267eba1ee376bbd1ad00d047c5f6a589.

--- src/updateUser.php
+++ src/updateUser.php
@@ -1,44 +1,37 @@
 <?php
 include "Userfnc.php";
-
-$id = isset($_REQUEST["id"]) ? intval($_REQUEST["id"]) : 0;
-
-if ($id <= 0) {
-	die("Invalid user ID.");
-}
-
+$id = $_REQUEST["id"];
 $Result = getUserById($id);
-
-if (!$Result) {
-	die("User not found.");
-}
-
-$Email = htmlspecialchars($Result['Email'], ENT_QUOTES, 'UTF-8');
-$pass = htmlspecialchars(Decrypt($Result['Password'], 2), ENT_QUOTES, 'UTF-8');
+$Email = $Result['Email'];
+$pass = Decrypt($Result['Password'], 2);
 ?>
 <form action="UpdateUser2.php" method="post">
 	<table>
+
 		<tr>
 			<td>Email</td>
-			<td><input disabled type="text" value="<?php echo $Email ?>" /></td>
+			<td><input disabled type="text" name="Dummy" value="<?php echo $Email ?>" /></td>
 		</tr>
 		<tr>
-			<td>Password</td>
+			<td>pass</td>
 			<td><input type="password" name="Password" value="<?php echo $pass ?>" /></td>
 		</tr>
+
 		<tr>
-			<td>Full Name</td>
-			<td><input type="text" name="FullName" value="<?php echo htmlspecialchars($Result['FullName'], ENT_QUOTES, 'UTF-8') ?>" /></td>
+			<td>FullName</td>
+			<td><input type="text" name="FullName" value="<?php echo $Result['FullName'] ?>" /></td>
 		</tr>
 		<tr>
 			<td>DOB</td>
-			<td><input type="date" name="DOB" value="<?php echo htmlspecialchars($Result['DOB'], ENT_QUOTES, 'UTF-8') ?>" /></td>
+			<td><input type="text" name="DOB" value="<?php echo $Result['DOB'] ?>" /></td>
 		</tr>
 		<tr>
 			<td><input type="submit" /></td>
 			<td><input type="reset" /></td>
+
 		</tr>
+		<input type="hidden" name="id" value="<?php echo $Result['id'] ?>">
+		<input type="hidden" name="Email" value="<?php echo $Email ?>" />
 	</table>
-	<input type="hidden" name="id" value="<?php echo $id ?>">
-	<input type="hidden" name="Email" value="<?php echo $Email ?>" />
+
 </form>
