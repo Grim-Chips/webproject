c8f8c8471c5e9d0528db208d0dd2400c7985d466
GrimChips <Ak48umar2@gmail.com>
2025-05-23 12:35:33 +0300

    hhehe

--- src/login.php
+++ src/login.php
@@ -2,5 +2,8 @@
 
 ?>
 <form action="doLogin.php" method="post">
-Email<input type="text" name="Email"/>Password<input type="password" name="Password" /><input type="submit" />
+    Email<input type="text" name="Email" /><br>Password<input type="password" name="Password" /><br>
+    <input type="submit" />
+    don't have an account? <a href="register.php">Register</a>
+    <br />
 </form>
