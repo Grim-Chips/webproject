78cc5d39267eba1ee376bbd1ad00d047c5f6a589
blazmaa <youssefselim2108@gmail.com>
2025-05-23 22:39:10 +0300

    cooomit

--- src/updateUser.php
+++ src/updateUser.php
@@ -1,37 +1,44 @@
 <?php
 include "Userfnc.php";
-$id = $_REQUEST["id"];
+
+$id = isset($_REQUEST["id"]) ? intval($_REQUEST["id"]) : 0;
+
+if ($id <= 0) {
+	die("Invalid user ID.");
+}
+
 $Result = getUserById($id);
-$Email = $Result['Email'];
-$pass = Decrypt($Result['Password'], 2);
+
+if (!$Result) {
+	die("User not found.");
+}
+
+$Email = htmlspecialchars($Result['Email'], ENT_QUOTES, 'UTF-8');
+$pass = htmlspecialchars(Decrypt($Result['Password'], 2), ENT_QUOTES, 'UTF-8');
 ?>
 <form action="UpdateUser2.php" method="post">
 	<table>
-
 		<tr>
 			<td>Email</td>
-			<td><input disabled type="text" name="Dummy" value="<?php echo $Email ?>" /></td>
+			<td><input disabled type="text" value="<?php echo $Email ?>" /></td>
 		</tr>
 		<tr>
-			<td>pass</td>
+			<td>Password</td>
 			<td><input type="password" name="Password" value="<?php echo $pass ?>" /></td>
 		</tr>
-
 		<tr>
-			<td>FullName</td>
-			<td><input type="text" name="FullName" value="<?php echo $Result['FullName'] ?>" /></td>
+			<td>Full Name</td>
+			<td><input type="text" name="FullName" value="<?php echo htmlspecialchars($Result['FullName'], ENT_QUOTES, 'UTF-8') ?>" /></td>
 		</tr>
 		<tr>
 			<td>DOB</td>
-			<td><input type="text" name="DOB" value="<?php echo $Result['DOB'] ?>" /></td>
+			<td><input type="date" name="DOB" value="<?php echo htmlspecialchars($Result['DOB'], ENT_QUOTES, 'UTF-8') ?>" /></td>
 		</tr>
 		<tr>
 			<td><input type="submit" /></td>
 			<td><input type="reset" /></td>
-
 		</tr>
-		<input type="hidden" name="id" value="<?php echo $Result['id'] ?>">
-		<input type="hidden" name="Email" value="<?php echo $Email ?>" />
 	</table>
-
+	<input type="hidden" name="id" value="<?php echo $id ?>">
+	<input type="hidden" name="Email" value="<?php echo $Email ?>" />
 </form>
