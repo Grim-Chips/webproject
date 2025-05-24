24f0b86a00780d9e81d348db6b91f4b2c95ee9a4
GrimChips <Ak48umar2@gmail.com>
2025-05-25 00:24:13 +0300

    Revert "kkk"
    This reverts commit 3e68792fbd4bd92e38e9de420d5f92accc3f08dd.
    
    h

--- src/login.php
+++ src/login.php
@@ -1,25 +1,12 @@
 <?php
-session_start();
 
-$users = json_decode(file_get_contents("data/users.json"), true);
-
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-$logged_in = false;
-
-for ($i = 0; $i < count($users); $i++) {
-  if ($users[$i]['username'] == $username && $users[$i]['password'] == $password) {
-    $_SESSION['username'] = $username;
-    $_SESSION['role'] = $users[$i]['role'];
-    $logged_in = true;
-    break;
-  }
-}
-
-if ($logged_in) {
-  echo json_encode(array("success" => true));
-} else {
-  echo json_encode(array("success" => false, "message" => "Wrong username or password"));
-}
 ?>
+<form action="dologin.php" method="post">
+
+  Email<input type="text" name="Email" />
+  Password<input type="password" name="Password" />
+  <input type="submit" />
+  <input type="reset" /><br>
+  donot have an account? <a href="RegisterationForm.php">Register</a>
+  <br />
+</form>
