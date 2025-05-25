bb5001420098b3011b18763d7c69cba233bb0a34
Grim-Chips <ak48umar2@gmail.com>
2025-05-25 09:17:52 +0300

    Refactor login.php to include HTML structure and improve layout

--- src/login.php
+++ src/login.php
@@ -1,7 +1,16 @@
 <?php
-
+include_once (__DIR__.'/assets/templates/header.php');
+Navigation();
 ?>
-<form action="dologin.php" method="post">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="dologin.php" method="post" style="text-align: center;">
 
   Email<input type="text" name="Email" />
   Password<input type="password" name="Password" />
@@ -10,3 +19,9 @@
   donot have an account? <a href="RegisterationForm.php">Register</a>
   <br />
 </form>
+</body>
+</html>
+<?php
+include_once (__DIR__.'/assets/templates/footer.php');
+Footer();
+?>
